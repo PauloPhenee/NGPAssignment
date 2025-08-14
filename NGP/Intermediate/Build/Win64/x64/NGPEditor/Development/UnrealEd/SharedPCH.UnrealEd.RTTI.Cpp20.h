7b125cabc540d34cc56da24ff21ad78f1bddfe25
PauloPhenee <paulo.phenee@gmail.com>
2025-08-14 14:07:15 -0300

    Init

---
+++ NGP/Intermediate/Build/Win64/x64/NGPEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "D:/User/Source/Repos/NGP/NGPAssignment/NGP/Intermediate/Build/Win64/x64/NGPEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
